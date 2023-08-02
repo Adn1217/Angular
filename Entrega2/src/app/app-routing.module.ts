a177323d0c785a83ca6c3d170edfe9dd4c93e843
Adrian Fernandez <77861592+Adn1217@users.noreply.github.com>
2023-08-01 22:48:30 -0500

    Segunda entrega

---
+++ Entrega2/src/app/app-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegistroComponent } from './formulario/registro/registro.component';
+import { IngresoComponent } from './formulario/ingreso/ingreso.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfesoresComponent } from './pages/profesores/profesores.component';
+
+const routes: Routes = [
+  { path: 'home',
+    component: HomeComponent,
+    children: [
+      // {
+      // path: 'register',
+      // component: RegistroComponent
+      // },
+      {
+        path: '**',
+        redirectTo: '/home' 
+      }
+    ]
+  },
+  {
+    path: 'login',
+    component: IngresoComponent
+  },
+  {
+    path: 'teachers',
+    component: ProfesoresComponent
+  },
+  {
+    path: 'register',
+    component: RegistroComponent
+  },     
+  {
+    path: '**',
+    redirectTo: '/login' 
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
